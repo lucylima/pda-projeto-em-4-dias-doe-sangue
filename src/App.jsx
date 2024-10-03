b080257655cc42f258f6549f78a63962f1b6c28a
Junior Reis <gilmaikjunior@gmail.com>
2024-10-03 20:05:31 -0300

    Second commt

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
+import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
   return (
     <>
-        <Nav
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+      <Navbar style={{backgroundColor: '#CD8585'}} data-bs-theme="dark">
+        <Container style={{display: "flex", justifyContent:'space-between'}}>
+          <Navbar.Brand href="#home">DSSMR</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Tipos Sanguineo</Nav.Link>
+            <Nav.Link href="#pricing">Localização</Nav.Link>
+            <Nav.Link href="#pricing">Contato</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
-  ) 
+  );
 }
 
-export default App
+export default App;
